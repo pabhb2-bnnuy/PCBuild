af3823a65b53e830dad746bb23bb3b8e7c6cf7f0
pabhb2-bnnuy <pabhb2@gmail.com>
2026-05-24 23:56:51 +0200

    cambiando imports

--- src/main/java/com/pcb/build/security/SecurityConfig.java
+++ src/main/java/com/pcb/build/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.pcb.build.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
